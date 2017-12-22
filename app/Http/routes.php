fcee10168841c319ad0366bc33b40026c6665541
dmittal1 <mittald@hotmail.co.uk>
2017-12-22 00:11:42 +0000

    done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,72 @@
 |
 */
 
+use App\Post;
+use App\Tag;
+use App\Video;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create',function(){
+
+  $post = Post::create(['name'=>'My first post']);
+
+  $tag1 = Tag::find(1);
+
+  $post->tags()->save($tag1);
+
+  $video = Video::create(['name'=>'video.mov']);
+
+  $tag2 = Tag::find(2);
+
+  $video->tags()->save($tag2);
+
+});
+
+Route::get('/read', function(){
+
+  $post = Post::findOrFail(1);
+
+  foreach($post->tags as $tag){
+
+    echo $tag;
+
+  }
+
+});
+
+Route::get('/update', function(){
+
+  // $post = Post::findOrFail(1);
+  //
+  // foreach($post->tags as $tag){
+  //
+  //   return $tag->whereName('PHP')->update(['name'=>'Updated PHP']);
+
+  //}
+
+  $post = Post::findOrFail(1);
+
+  $tag = Tag::find(3);
+
+  //$post->tags()->save($tag);
+
+  //$post->tags()->attach($tag);
+
+  $post->tags()->sync([1,2]);
+
+});
+
+Route::get('/delete', function(){
+
+  $post = Post::find(1);
+
+  foreach($post->tags as $tag){
+
+    $tag->whereId(4)->delete();
+
+  }
+
 });
